94e192ed32448f92c9cfcb53ff7c6f5f53dd86f4
XunjieLiu <634629477@qq.com>
2021-02-25 01:44:47 -0800

    add README

---
+++ src/Exam_0225/Main.java
@@ -0,0 +1,9 @@
+package Exam_0225;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello, this is a test");
+    }
+
+}
---
+++ src/Exam_0225/README.md
@@ -0,0 +1,5 @@
+# README
+
+This is a little Java packages for lab exam in 2021/02/25
+
+Need to be updated ...
